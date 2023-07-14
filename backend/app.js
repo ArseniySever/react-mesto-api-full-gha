d2d0fe273bf4fe68665dda6382a287a98b163000
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-07-14 10:31:59 +0300

    feat: авторизация и подзагрузка карточек

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ const { login, createUser } = require('./controllers/users');
 
 const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
+
 mongoose
   .connect(DB_URL, {
     useNewUrlParser: true,
