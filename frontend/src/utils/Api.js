a23b97f266a9a7642464340c593b1e717790e3ec
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-07-18 20:36:15 +0300

    feat

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -74,7 +74,7 @@ class Api {
   }
 }
 const apiSettings = {
-  baseUrl: 'https://api.domainname.students.n.nomoredomains.work',
+  baseUrl: 'http://api.domainname.students.n.nomoredomains.work',
 
   headers: {
     "Content-Type": "application/json",
