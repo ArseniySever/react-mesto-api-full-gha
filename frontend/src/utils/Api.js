b58877e390e966ddf44e89142f14367d7c392808
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-07-20 09:23:15 +0300

    feat: добавить адаптив для мобильных и планшетов

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -75,7 +75,7 @@ class Api {
   }
 }
 const apiSettings = {
-  baseUrl: 'http://api.domainname.students.n.nomoredomains.work',
+  baseUrl: 'https://api.domainname.students.n.nomoredomains.work',
 
   headers: {
     "Content-Type": "application/json",
